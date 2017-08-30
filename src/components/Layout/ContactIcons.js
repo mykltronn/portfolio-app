31e3f4ce70df915ab0acc073893e970c3b9169ae
Michael Ashton <mykl.ashton@gmail.com>
2017-08-30 10:12:12 -0400

    Begin animation of email dispay, fix disappearing navmenu

--- src/components/Layout/ContactIcons.js
+++ src/components/Layout/ContactIcons.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import EmailDrop from './EmailDrop.js';
 
-const ContactIcons = (props) => {
-  return (
-    <ul className={"ContactIcons " + (props.menuActive ? "icons-active" : '')}>
-      <li>
-        <a href="https://github.com/mykltronn" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/m-ashton/" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-      </li>
-      <li>
-        <i className="fa fa-envelope" aria-hidden="true"></i>
-      </li>
-    </ul>
-  )
+export default class ContactIcons extends Component {
+  constructor() {
+    super()
+
+    this.toggleEmail = this.toggleEmail.bind(this);
+
+    this.state = {
+      showEmail: false
+    }
+  }
+
+  toggleEmail() {
+    this.setState({ showEmail: !this.state.showEmail})
+  }
+  render () {
+    return (
+      <ul className={"ContactIcons " + (this.props.menuActive ? "icons-active" : '')}>
+        <li>
+          <a href="https://github.com/mykltronn" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/m-ashton/" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        </li>
+        <li
+          className="email"
+          onClick={this.toggleEmail}
+          showEmail={this.state.showEmail}>
+          <i className="fa fa-envelope" aria-hidden="true"></i>
+        </li>
+      </ul>
+    )
+  }
 }
-
-export default ContactIcons;
---
+++ src/components/Layout/EmailDrop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmailDrop = (props) => {
+  return (
+    <div className={"EmailDrop " + (props.showEmail ? "show-email": '')}>
+
+    </div>
+  )
+}
+
+export default EmailDrop;
