db69fd8ea675a71aa0f81b8c3844afb8e0df42be
Michael Ashton <mykl.ashton@gmail.com>
2017-08-30 00:56:37 -0400

    Begin portfolio page design

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -10,19 +10,17 @@ export const Project = (props) => {
   return (
     <div className="Project">
       <div className="project-image">
-          <a href={project.webLink}><img src={project.image}/></a>
+        <a href={project.webLink}><img src={project.image}/></a>
       </div>
-      <div className="project-deets-wrap">
-        <div className="project-details">
-          <h2>{project.name}</h2>
-          <p>{project.description}</p>
-          <h3>Built Using</h3>
-          <p>{project.languages}</p>
-          <ul className="project-links">
-              <li><a href={project.webLink} target="_blank" rel="noopener noreferrer">view project</a></li>
-              <li><a href={project.gitLink} target="_blank" rel="noopener noreferrer">github</a></li>
-          </ul>
-        </div>
+      <div className="project-details">
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        <h3>Built Using</h3>
+        <p>{project.languages}</p>
+        <ul className="project-links">
+          <li><a href={project.webLink} target="_blank" rel="noopener noreferrer">view project</a></li>
+          <li><a href={project.gitLink} target="_blank" rel="noopener noreferrer">github</a></li>
+        </ul>
       </div>
     </div>
   )
