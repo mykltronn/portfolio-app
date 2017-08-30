ed6d34e64338aa6d7b0f17ef000b2531134154f8
Michael Ashton <mykl.ashton@gmail.com>
2017-08-30 11:38:14 -0400

    Add email pop-out with copy-to-clipboard functionality

--- src/components/Layout/ContactIcons.js
+++ src/components/Layout/ContactIcons.js
@@ -24,11 +24,10 @@ export default class ContactIcons extends Component {
         <li>
           <a href="https://www.linkedin.com/in/m-ashton/" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
         </li>
-        <li
-          className="email"
-          onClick={this.toggleEmail}
-          showEmail={this.state.showEmail}>
-          <i className="fa fa-envelope" aria-hidden="true"></i>
+        <li className="email" onClick={this.toggleEmail}>
+          <i className="fa fa-envelope" aria-hidden="true">
+            <EmailDrop showEmail={this.state.showEmail} toggleEmail={this.toggleEmail}/>
+          </i>
         </li>
       </ul>
     )
--- src/components/Layout/EmailDrop.js
+++ src/components/Layout/EmailDrop.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const EmailDrop = (props) => {
-  return (
-    <div className={"EmailDrop " + (props.showEmail ? "show-email": '')}>
+export default class EmailDrop extends Component {
 
-    </div>
-  )
+  copyEmail (event){
+    let element = event.target.dataset.copytarget;
+    let target = document.querySelector(element);
+    let email = target.innerText; // email = "mykl.ashton@gmail.com"
+    let tempInput = document.createElement('textarea');
+    // create temporary 'textarea'. .select() only grabs text from input or textarea.
+
+    tempInput.innerText = email // assign email to input
+    document.body.appendChild(tempInput) // build input with data assigned
+    tempInput.select() // run .select() on temp textarea
+    document.execCommand('copy') // use copy api to copy selected text
+    tempInput.remove() // remove temporary text area
+  }
+
+  render () {
+    return (
+      <div className={"EmailDrop " + (this.props.showEmail ? "show-email": '')} onClick={this.props.toggleEmail}>
+        <span id="email-address">mykl.ashton@gmail.com</span>
+        <button
+          data-copytarget="#email-address"
+          id="copy-button"
+          onClick={this.copyEmail.bind(this)}>copy</button>
+      </div>
+    )
+  }
 }
-
-export default EmailDrop;
