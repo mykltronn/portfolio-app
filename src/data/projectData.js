713bb32162ba96f17702ca0debcb1106a898ee10
Michael Ashton <mykl.ashton@gmail.com>
2017-09-16 11:00:52 -0400

    Add Airbnb to porfolio

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,37 +1,46 @@
 const projects = [
-    {
-        name: "Etsy Clone",
-        description: "A clone of the popular ecommerce site, Etsy. My group re-created the homepage and header. I created the dropdown navbar from scratch as well as a number of other components on the page.",
-        languages: "React",
-        webLink: "https://mykltronn.github.io/Team-Homely/",
-        groupLink: "",
-        gitLink: "https://github.com/mykltronn/Team-Homely",
-        image: "./img/etsy.png"
-    },
-    {
-        name: "NuÜ",
-        description: "NuÜ (pronounced New You) is an ecommerce site for the social-media challanged. The initial version of the site was a group effort, while the Redux refactoring is my own work. My contributions to the collaborative effort include the shopping page with slider, built without a library.",
-        languages: "Node.js, React, Redux",
-        webLink:"",
-        gitLink:"https://github.com/mykltronn/redux-iron-store",
-        image: "./img/NuU.jpeg"
-    },
-    {
-        name: "Snippet Organizer",
-        description: "An App and API for performing CRUD operations on a database of code snippets. API documenation is included on the home page. Users can also add snippets from the web app.",
-        languages: "Node.js, JavaScript, Mustache, MongoDB, Mongoose, Heroku",
-        webLink: "https://floating-river-54851.herokuapp.com/",
-        gitLink: "https://github.com/mykltronn/snippet-organizer",
-        image: "./img/snippet.png"
-    },
-    // {
-    //     name: "Mars Rover",
-    //     description: "",
-    //     languages: "React",
-    //     webLink: "",
-    //     gitLink: "https://github.com/mykltronn/mars-rover-photo-app",
-    //     image: "",
-    // }
+  {
+    name: "Airbnb Listing Clone",
+    description: "I had six days to complete this clone. The challenge was to create an MVP duplicating a particular Airbnb listing. I used several libraries, built a few carousels by hand and custom-built the 'Booking' widget in the sidebar.",
+    languages: "React, Redux",
+    webLink: "https://mykltronn.github.io/airbnb-clone/",
+    groupLink: "",
+    gitLink: "https://github.com/mykltronn/airbnb-clone",
+    image: "./img/airbnb.jpeg"
+  },
+  {
+      name: "Etsy Clone",
+      description: "A clone of the popular ecommerce site, Etsy. My group re-created the homepage and header. I created the dropdown navbar from scratch as well as a number of other components on the page.",
+      languages: "React"
+      webLink: "https://mykltronn.github.io/Team-Homely/",
+      groupLink: "",
+      gitLink: "https://github.com/mykltronn/Team-Homely",
+      image: "./img/etsy.png"
+  },
+  {
+      name: "NuÜ",
+      description: "NuÜ (pronounced New You) is an ecommerce site for the social-media challanged. The initial version of the site was a group effort, while the Redux refactoring is my own work. My contributions to the collaborative effort include the shopping page with slider, built without a library.",
+      languages: "Node.js, React, Redux",
+      webLink:"",
+      gitLink:"https://github.com/mykltronn/redux-iron-store",
+      image: "./img/NuU.jpeg"
+  },
+  {
+      name: "Snippet Organizer",
+      description: "An App and API for performing CRUD operations on a database of code snippets. API documenation is included on the home page. Users can also add snippets from the web app.",
+      languages: "Node.js, JavaScript, Mustache, MongoDB, Mongoose, Heroku",
+      webLink: "https://floating-river-54851.herokuapp.com/",
+      gitLink: "https://github.com/mykltronn/snippet-organizer",
+      image: "./img/snippet.png"
+  },
+  // {
+  //     name: "Mars Rover",
+  //     description: "",
+  //     languages: "React",
+  //     webLink: "",
+  //     gitLink: "https://github.com/mykltronn/mars-rover-photo-app",
+  //     image: "",
+  // }
 ]
 
 export default projects;
