a5a86a40db14161287c26d75b70f9517126055a8
Michael Ashton <mykl.ashton@gmail.com>
2018-02-06 20:17:10 -0500

    Adjust projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,28 +1,49 @@
 export const skills = [
-  {name: "HTML5", sub: null},
-  {name: "CSS3", sub: [
-    "Stylus",
-    "Flexbox",
-    "Grid"
+  {name: "React", sub: [
+    "Redux",
+    "Sagas",
+    "React Router 4.0"
   ]},
-  {name: "Javascript", sub: [
-    "ES2016"
-  ]},
-  {name: "React", sub: null},
-  {name: "Redux", sub: [
-    "Redux-Thunk"
-  ]},
-  {name: "Node.js", sub: [
+  { name: "HTML5", sub: null },
+  {
+    name: "CSS3", sub: [
+      "Stylus",
+      "Flexbox",
+      "Grid"
+    ]
+  },
+  {
+    name: "Javascript", sub: [
+      "ES6",
+      "ES7"
+    ]
+  },
+  {
+    name: "Node.js", sub: [
     "Express",
-    "Mustache"
-  ]},
-  {name: "MongoDB", sub: [
+    "Events API",
+    "Websocket API"
+    ]
+  },
+  { name: "Ruby on Rails", sub: null },
+  { 
+    name: "Elixir", sub: [
+    "Phoenix 1.3"
+    ]
+  },
+  {name: "NoSQL", sub: [
+    "MongoDB",
     "Mongoose"
   ]},
   {name: "SQL", sub: [
-    "Postgresql"
+    "Postgresql",
+    "MySQL",
+    "Sequelize"
   ]},
-  {name: "GitHub", sub: null}
+  {name: "Git", sub: [
+    "GitHub",
+    "Bitbucket"
+  ]}
 ]
 
 export const projects = [
@@ -44,22 +65,22 @@ export const projects = [
       gitLink: "https://github.com/mykltronn/Team-Homely",
       image: "./img/etsy.png"
   },
-  {
-      name: "NuÜ",
-      description: "NuÜ (pronounced New You) is an ecommerce site for the social-media challanged. The initial version of the site was a group effort, while the Redux refactoring is my own work. My contributions to the collaborative effort include the shopping page with slider, built without a library.",
-      languages: "Node.js, React, Redux",
-      webLink:"",
-      gitLink:"https://github.com/mykltronn/redux-iron-store",
-      image: "./img/NuU.jpeg"
-  },
-  {
-      name: "Snippet Organizer",
-      description: "An App and API for performing CRUD operations on a database of code snippets. API documenation is included on the home page. Users can also add snippets from the web app.",
-      languages: "Node.js, JavaScript, Mustache, MongoDB, Mongoose, Heroku",
-      webLink: "https://floating-river-54851.herokuapp.com/",
-      gitLink: "https://github.com/mykltronn/snippet-organizer",
-      image: "./img/snippet.png"
-  },
+  // {
+  //     name: "NuÜ",
+  //     description: "NuÜ (pronounced New You) is an ecommerce site for the social-media challanged. The initial version of the site was a group effort, while the Redux refactoring is my own work. My contributions to the collaborative effort include the shopping page with slider, built without a library.",
+  //     languages: "Node.js, React, Redux",
+  //     webLink:"",
+  //     gitLink:"https://github.com/mykltronn/redux-iron-store",
+  //     image: "./img/NuU.jpeg"
+  // },
+  // {
+  //     name: "Snippet Organizer",
+  //     description: "An App and API for performing CRUD operations on a database of code snippets. API documenation is included on the home page. Users can also add snippets from the web app.",
+  //     languages: "Node.js, JavaScript, Mustache, MongoDB, Mongoose, Heroku",
+  //     webLink: "https://floating-river-54851.herokuapp.com/",
+  //     gitLink: "https://github.com/mykltronn/snippet-organizer",
+  //     image: "./img/snippet.png"
+  // },
   // {
   //     name: "Mars Rover",
   //     description: "",
